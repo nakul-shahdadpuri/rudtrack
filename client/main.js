2841729006e8c7433b3c06c0cc12462bed445fb6
nakul-shahdadpuri <nakulshahdadpuri3141@gmail.com>
2021-09-05 13:27:44 +0530

    added client template

---
+++ client/main.js
@@ -0,0 +1,68 @@
+const { ipcMain } = require('electron');
+const electron = require('electron');
+const fs = require('fs');
+const { exit } = require('process');
+const {dialog} = electron;
+const app = electron.app;
+const Window = electron.BrowserWindow;
+const {ipc} = electron;
+const ts = require("typescript")
+
+let win;
+let win2;
+
+app.on("ready",() => {
+    win = new Window({
+        width:600,
+        height:600,
+        minHeight:600,
+        minWidth:600,
+        maxHeight:600,
+        maxWidth:600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    win.loadFile('index.html')
+})
+
+
+//communication with renderer
+
+ipcMain.on('execute',(event,text) => {
+
+    // let sourceCode = text;
+    // let tsSourceFile = ts.createSourceFile(
+    //     __filename,
+    //     sourceCode,
+    //     ts.ScriptTarget.Latest
+    //   );
+    // console.log(tsSourceFile);
+    let x = text.indexOf("createCanvas");
+    console.log(x);
+
+    fs.writeFileSync('template/sketch.js',text,(err) => {
+        if(err){
+            console.log('BUFFER FILE ERROR');
+        };
+    })
+
+    win2 = new Window({
+        width: 600,
+        height:600
+    });
+    win2.loadFile('template/test.html');
+})
+
+ipcMain.on('save', (event,text)=> {
+    let path = dialog.showSaveDialogSync(win,{defaultPath: "save.txt"});
+    if(path)
+    {
+        fs.writeFile(path,text,(err)=>{
+            if(err){
+                console.log("ERROR");
+            }
+        console.log("File saved.")
+    })
+    }
+});
